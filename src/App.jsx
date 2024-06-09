c69c257188b1848a76f870ca667847460b0ff3c1
nusratnova5 <nusrat.nova16@gmail.com>
2024-06-09 12:09:18 +0600

    structure done

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import About from "./Components/About/About"
+import Banner from "./Components/Banner/Banner"
+import Contact from "./Components/Contact/Contact"
+import Footer from "./Components/Footer"
+import Header from "./Components/Header"
+import Projects from "./Components/Projects/Projects"
+import Skills from "./Components/Skills/Skills"
+
+function App() {
+
+  return (
+    <div className="w-3/4 mx-auto">
+      <Header/>
+      <Banner/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
