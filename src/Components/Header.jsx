2d83463ff22ba728af575711df5e64f117a4c031
nusratnova5 <nusrat.nova16@gmail.com>
2024-06-09 13:48:33 +0600

    projects section added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,24 +2,16 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-sky-900 text-white">
     <div className="flex-1">
-      <a className="btn btn-ghost text-xl">daisyUI</a>
+      <a className="btn btn-ghost text-xl">Nusrat</a>
     </div>
     <div className="flex-none">
       <ul className="menu menu-horizontal px-1">
-        <li><a>Link</a></li>
-        <li>
-          <details>
-            <summary>
-              Parent
-            </summary>
-            <ul className="p-2 bg-base-100 rounded-t-none">
-              <li><a>Link 1</a></li>
-              <li><a>Link 2</a></li>
-            </ul>
-          </details>
-        </li>
+        <li><a>About</a></li>
+        <li><a>Skills</a></li>
+        <li><a>Projects</a></li>
+        <li><a>Contact</a></li>        
       </ul>
     </div>
   </div>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import img1 from '../Projects/images/project-1.jpg';
+import img2 from '../Projects/images/project-2.jpg';
+import { BsArrowUpRightCircleFill } from "react-icons/bs";
 
 const Projects = () => {
     return (
-        <div>
-            This is projects
+        <div className="text-center p-8 my-20">
+            <h2 className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-sky-900">
+                Explore My Projects
+            </h2>
+
+            <div className="flex flex-wrap items-center mt-10 text-left ">
+                <div className="w-full md:w-3/5 lg:w-1/2 px-4">
+                    <img src={img1} alt="gem" className="inline-block rounded shadow-lg border border-merino-400" />
+                </div>
+                <div className="w-full md:w-2/5 lg:w-1/2 px-4 text-center md:text-left lg:pl-12">
+                    <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
+                        TechHub
+                    </h3>
+                    <p className="sm:text-lg my-6">
+                        TechHub is an e-commerce site offering a wide range of high-quality tech accessories. Enjoy a seamless shopping experience with our intuitive design, secure payments, and fast shipping.                </p>
+
+                    <a href="https://tech-hub-front-pearl.vercel.app/"><BsArrowUpRightCircleFill className='text-sky-900 text-4xl' /></a>
+                </div>
+
+            </div>
+
+            <div className="flex flex-wrap items-center mt-20 text-left">
+                <div className="w-full md:w-3/5 lg:w-1/2 px-4">
+                    <img src={img2} alt="project members" className="inline-block rounded shadow-lg border border-merino-400" />
+                </div>
+                <div className="w-full md:w-2/5 lg:w-1/2 px-4 md:order-first text-center md:text-left lg:pr-12">
+                    <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
+                        Food Delivery Website
+                    </h3>
+                    <p className="sm:text-lg my-6">
+                    This project is a food delivery website created as part of a design challenge for a Web Developer Internship. The website is built using HTML, CSS, Tailwind CSS, daisyUI, SwiperJS, and Google Fonts.                    </p>
+                
+                    <a href=""><BsArrowUpRightCircleFill className='text-sky-900 text-4xl' /></a>
+                </div>
+            </div>
         </div>
     );
 };
